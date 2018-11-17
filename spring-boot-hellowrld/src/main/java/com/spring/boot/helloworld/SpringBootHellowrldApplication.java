dfbbb16a1df2f64bc5131a252531c76f95fdd375
dudutushu <dudutushu@163.com>
2018-11-17 17:11:51 +0800

    spring-boot-hellowrld project commit

---
+++ spring-boot-hellowrld/src/main/java/com/spring/boot/helloworld/SpringBootHellowrldApplication.java
@@ -0,0 +1,16 @@
+package com.spring.boot.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @SpringBootApplication 来标注一个主程序类，说明这是一个Spring Boot应用
+ */
+@SpringBootApplication
+public class SpringBootHellowrldApplication {
+
+    public static void main(String[] args) {
+        // Spring应用启动起来
+        SpringApplication.run(SpringBootHellowrldApplication.class, args);
+    }
+}
